ff2cbdef5643fde2c3e958cd24d3258c6560cf2c
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-25 23:47:20 +0530

    registration of user completed created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,19 +3,20 @@ const userService = require("../services/user.service");
 const { validationResult} = require("express-validator");
 
 
-exports.registerUser = async(req,req,next)=>{
+exports.registerUser = async(req,res,next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array()});
     }
 
-    const {firstname,lastname,email,password} = req.body;
+    const {fullname,email,password} = req.body;
+    console.log(req.body)
 
     const hashedPassword = await User.hashPassword(password);
 
     const user = await userService.createUser({
-        firstname,
-        lastname,
+        firstname:fullname.firstname,
+        lastname:fullname.lastname,
         email,
         password:hashedPassword
     });
