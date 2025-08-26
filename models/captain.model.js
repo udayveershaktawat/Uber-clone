aa0db9cb1cf8ec1e675861c54308a04e62c678fe
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-26 23:54:15 +0530

    methods are created

--- models/captain.model.js
+++ models/captain.model.js
@@ -1,75 +1,86 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const captainSchema = new mongoose.Schema({
-    fullname:{
-        firstname:{
-            type:String,
-            required:true,
-            minlength:[3,"first name must be at least 3 character"]
-        },
-        lastname:{
-            type:String,
-            required:true,
-            minlength:[3,"last name must be at least 3 character"]
-        }
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "first name must be at least 3 character"],
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true
+    lastname: {
+      type: String,
+      required: true,
+      minlength: [3, "last name must be at least 3 character"],
     },
-    password:{
-        type:String,
-        required:true,
-        select:false
-    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 
-    socketId:{
-        type:String,
-        // required:true
+  socketId: {
+    type: String,
+    // required:true
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "inactive",
+  },
+  vehicle: {
+    color: {
+      type: String,
+      required: true,
+      minlength: [3, "color must be at least 3 characters long"],
     },
-    status:{
-        type:String,
-        enum:['active',"inactive"],
-        default:"inactive"
+    plate: {
+      type: String,
+      required: true,
+      minlength: [3, "plate must be at least 3 characters long"],
     },
-    vehicle:{
-        color:{
-            type:String,
-            required:true,
-            minlength:[3,'color must be at least 3 characters long'],
-        },
-        plate:{
-            type:String,
-            required:true,
-            minlength:[3,'plate must be at least 3 characters long']
-        },
-        capacity:{
-            type:Number,
-            required:true,
-            min:[1,"capacity must be at least 1"]
-        },
-        vehicleType:{
-            type:String,
-            required:true,
-            enum:['car','motorcycle','auto']
-        }
+    capacity: {
+      type: Number,
+      required: true,
+      min: [1, "capacity must be at least 1"],
     },
-    location:{
-        lat:{
-            type:Number
-        },
-        lng:{
-            type:Number
-        }
-    }
-
-
-    
-        
-    
-})
+    vehicleType: {
+      type: String,
+      required: true,
+      enum: ["car", "motorcycle", "auto"],
+    },
+  },
+  location: {
+    lat: {
+      type: Number,
+    },
+    lng: {
+      type: Number,
+    },
+  },
+});
 
 
-module.exports = mongoose.model("Captain",captainSchema);
+captainSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET,{expiresIn:'24h'})
+    return token
+}
+
+captainSchema.methods.comparePassword = async function(){
+    return await bcrypt.compare(password,this.password);
+}
+
+captainSchema.statics.hashPassword = async function(){
+    return await bcrypt.hash(password,10);
+}
+
+
+module.exports = mongoose.model("Captain", captainSchema);
