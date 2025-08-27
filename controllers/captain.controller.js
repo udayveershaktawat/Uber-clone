a040eae0919f123978b386fd50d16b9fd71c5d2b
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-27 23:48:22 +0530

    captain controller register

--- controllers/captain.controller.js
+++ controllers/captain.controller.js
@@ -1,1 +1,44 @@
 const Captain = require("../models/captain.model");
+const captainService = require("../services/captain.service");
+const {validationResult} = require("express-validator")
+
+
+
+exports.registerCaptain = async(req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors,isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const {fullname,email,password,vehicle} = req.body;
+
+    const isCaptainAlreadyExist = await Captain.findOne({email});
+    if(isCaptainAlreadyExist){
+        return res.status(400).json({message:"captain already exists"})
+    }
+
+    const hashedPassword = await Captain.hashPassword(password);
+
+
+    const captain = await captainService.createCaptain({
+        firstname:fullname.firstname,
+        lastname:fullname.lastname,
+        email,
+        password:hashedPassword,
+        color:vehicle.color,
+        color:vehicle.capacity,
+        color:vehicle.plate,
+        color:vehicle.vehicleType,
+    });
+
+    const token = await captain.generateAuthToken();
+
+
+    return res.status(201).json({token,captain})
+
+
+
+
+
+
+}
