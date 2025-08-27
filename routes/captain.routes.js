a54d2c4846e7171065757c6abbe84708fb629d23
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-27 23:28:17 +0530

    route captain

--- routes/captain.routes.js
+++ routes/captain.routes.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const {body} = require("express-validator");
+const captainController = require("../controllers/captain.controller");
+
+
+
+
+router.post("/register",[
+    body("email").isEmail().withMessage("invalid email"),
+    body("fullname.firstname").isLength({min:3}).withMessage('first name must be at least 3 character'),
+    body("password").isLength({min:6}).withMessage('password must be at least 6 character'),
+    body("vehicle.color").isLength({min:3}).withMessage('color must be at least 3 character'),
+    body("vehicle.plate").isLength({min:3}).withMessage('color must be at least 3 character'),
+    body("vehicle.capacity").isLength({min:1}).withMessage('color must be at least 3 character'),
+    body("vehicle.vehicleType").isIn(['car',"auto","motorcycle"]).withMessage('invalid type'),
+],captainController.registerCaptain)
 
 
 
@@ -7,4 +22,4 @@ const router = express.Router();
 
 
 
-module.exports = router
+module.exports = router;
