6557c0bf23506dfc58d0f979cbe29e9610c209b0
udayveershaktawat <udayveershaktawat@gmail.com>
2025-08-28 23:22:56 +0530

    authcaptain

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -26,5 +26,44 @@ exports.authUser = async (req, res, next) => {
 };
 
 
+// captain authorization
+exports.authCaptain = async(req,res,next)=>{
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+  if(!token){
+    return res.status(401).json({message:"unauthorized"});
+  }
+
+  const isBlacklisted = await blacklistToken.findOne({token:token});
+
+
+  if(isBlacklisted){
+    return res.status(401).json({message:"unauthorized"});
+  }
+
+
+  try{
+
+    const decoded = await jwt.verify(token,process.env.JWT_SECRET);
+    const captain = await Captain.findById(decoded._id);
+    req.captain = captain;
+
+  }
+  catch(error){
+
+    res.status(401).json({message:"unauthorized"})
+
+
+  }
+
+
+
+
+
+
+
+
+}
+
+
 
 
