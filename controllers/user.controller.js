de36e7285c74b140705099e45fe76b101f86be75
udayveershaktawat <udayveershaktawat@gmail.com>
2025-09-08 23:47:16 +0530

    user controller updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,84 +1,74 @@
 const User = require("../models/user.model");
 const userService = require("../services/user.service");
-const { validationResult} = require("express-validator");
-const blacklistToken = require("../models/blacklistToken.model")
+const { validationResult } = require("express-validator");
+const blacklistToken = require("../models/blacklistToken.model");
 
+exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-exports.registerUser = async(req,res,next)=>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
-    }
+  const { fullname, email, password } = req.body;
+  console.log(req.body);
 
-    const {fullname,email,password} = req.body;
-    console.log(req.body)
+  const isUserAlreadyExist = await Captain.findOne({ email });
+  if (isUserAlreadyExist) {
+    return res.status(400).json({ message: "User already exists" });
+  }
 
+  const hashedPassword = await User.hashPassword(password);
 
-    
-        const isUserAlreadyExist = await Captain.findOne({email});
-        if(isUserAlreadyExist){
-            return res.status(400).json({message:"User already exists"})
-        }
+  const user = await userService.createUser({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
+  });
 
-    const hashedPassword = await User.hashPassword(password);
+  const token = user.generateAuthToken();
 
-    const user = await userService.createUser({
-        firstname:fullname.firstname,
-        lastname:fullname.lastname,
-        email,
-        password:hashedPassword
-    });
-
-    const token = user.generateAuthToken();
-
-    res.status(201).json({token,user});
-
-    
-}
+  res.status(201).json({ token, user });
+};
 
 // login
-exports.loginUser = async(req,res,next)=>{
-    const errors = validationResult(req);
+exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()})
-    }
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
+  const { email, password } = req.body;
 
-    const {email,password} = req.body;
+  const user = await User.findOne({ email }).select("+password");
 
-    const user = await User.findOne({email}).select('+password');
+  if (!user) {
+    return res.status(401).json({ message: "invalid email and password" });
+  }
 
-    if(!user){
-        return res.status(401).json({message:"invalid email and password"});
-    }
+  const isMatch = await user.comparePassword(password);
 
-    const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "invalid email and password" });
+  }
 
-    if(!isMatch){
-        return res.status(401).json({message:"invalid email and password"})
-    }
-
-    const token = user.generateAuthToken();
-    res.cookie("token",token);
-    return res.status(200).json({token,user})
-
-
-
-
-} 
+  const token = user.generateAuthToken();
+  res.cookie("token", token);
+  return res.status(200).json({ token, user });
+};
 
 // get profile
-exports.getUserProfile = async(req,res,next)=>{
-    res.status(200).json(req.user)
-}
+exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+};
 
 // logout
-exports.logoutUser = async(req,res,next)=>{
-    res.clearCookie('token');
-    const token = req.cookies.token || req.hearders.authorization?.split(' ')[1]
+exports.logoutUser = async (req, res, next) => {
+  res.clearCookie("token");
+  const token = req.cookies.token || req.hearders.authorization?.split(" ")[1];
 
-    await blacklistToken.create({token})
+  await blacklistToken.create({ token });
 
-    res.status(200).json({message:"logged out"})
-}
+  res.status(200).json({ message: "logged out" });
+};
