80c7539be85fbf4af30f11434c1085f40eacb78c
udayveershaktawat <udayveershaktawat@gmail.com>
2025-09-08 23:49:34 +0530

    updated service

--- services/user.service.js
+++ services/user.service.js
@@ -1,31 +1,18 @@
 const User = require("../models/user.model");
 
+exports.createUser = async ({ firstname, lastname, email, password }) => {
+  if (!firstname || !email || !password) {
+    throw new Error("all fields are required");
+  }
 
-exports.createUser = async({
-    firstname,lastname,email,password
-})=>{
-    if(!firstname || !email || !password){
-        throw new Error('all fields are required')
-    }
+  const user = await User.create({
+    fullname: {
+      firstname,
+      lastname,
+    },
+    email,
+    password,
+  });
 
-    const user = await User.create({
-        fullname:{
-            firstname,
-            lastname,
-
-        },
-        email,
-        password
-    })
-
-
-    return user
-
-
-
-
-
-
-
-
-}
+  return user;
+};
