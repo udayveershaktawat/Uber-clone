4259edb54e2534d9eac929f8612e7077503cd0e0
udayveershaktawat <udayveershaktawat@gmail.com>
2025-12-23 22:25:04 +0530

    user created successfully

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,30 @@ const {validationResult } = require("express-validator");
 module.exports.registerUser = async(req,res,next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json({errors:error.array()})
+        return res.status(400).json({errors:error.array()});
     }
+
+    const {firstName,lastName,email,password} = req.body;
+
+    const hashedpassword = await User.hashPassword(password);
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password:hashedpassword
+    });
+
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({
+        token,user
+    })
+
+
+
+
+
 
 }
