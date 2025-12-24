333ea212d0ed7e91cd4d3034b4e265fb56f24ff5
udayveershaktawat <udayveershaktawat@gmail.com>
2025-12-24 23:42:35 +0530

    updated app file

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-const dbconnect = require("./db");
-dbconnect();
+const dbconnect = require("./db/db");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoutes = require("./routes/user.routes")
+
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -9,6 +12,16 @@ const cors = require("cors");
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.use("/users",userRoutes)
+
+
+
+dbconnect();
 
 
 
