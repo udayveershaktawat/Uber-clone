0febdef7a95d4192d2374b31ef0aedede904739c
udayveershaktawat <udayveershaktawat@gmail.com>
2025-12-28 22:48:28 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,53 +1,30 @@
 const User = require("../models/user.model");
 const userService = require("../services/user.service");
-const {validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: error.array() });
+  }
 
-module.exports.registerUser = async(req,res,next)=>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:error.array()});
-    }
+  const { firstName, lastName, email, password } = req.body;
 
-    const {firstName,lastName,email,password} = req.body;
+  const hashedpassword = await User.hashPassword(password);
 
-    const hashedpassword = await User.hashPassword(password);
+  const user = await userService.createUser({
+    firstname,
+    lastname,
+    email,
+    password: hashedpassword,
+  });
 
-    const user = await userService.createUser({
-        firstname,
-        lastname,
-        email,
-        password:hashedpassword
-    });
+  const token = user.generateAuthToken();
 
+  res.status(201).json({
+    token,
+    user,
+  });
+};
 
-    const token = user.generateAuthToken();
-
-    res.status(201).json({
-        token,user
-    })
-
-
-
-
-
-
-}
-
-
-module.exportsloginUser = async(req,res,next)=>{
-    
-
-
-
-
-    
-
-
-
-
-
-
-
-    
-}
+module.exportsloginUser = async (req, res, next) => {};
