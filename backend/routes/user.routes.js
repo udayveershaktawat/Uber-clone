aa353ffc108e34492b581fde9f043bd3801f66a2
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-03 02:50:50 +0530

    login route created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -12,5 +12,11 @@ router.post("/register",[
     body("password").isLength({min:6}).withMessage("password must be at least 6 character long")
 ],userController.registerUser)
 
+// login
+router.post("/login",[
+    body("email").isEmail().withMessage("invalid email"),
+    body("password").isLength({min:6}).withMessage("password must be at least 6 character")
+],userController.loginUser)
+
 
 module.exports = router;
