9fa79061a89b10c18a0c094e16036cdc6b36d13d
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-04 23:38:52 +0530

    register captain controller created

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,2 +1,47 @@
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
+const {validationResult} = require("express-validator");
+
+
+
+module.exports.registerCaptain = async(req,res,next)=>{
+    try{
+
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+    const {fullname,email,password,vehicle} = req.body;
+
+    const hashedPassword = await captainModel.hashPassword(password);
+
+    const captain = await captainService.createCaptain({
+        firstname:fullname.firstname,
+        lastname:fullname.lastname,
+        email,
+        password:hashedPassword,
+        color:vehicle.color,
+        plate:vehicle.plate,
+        capacity:vehicle.capacity,
+        vehicleType:vehicle.vehicleType
+
+    })
+
+    const token = captain.generateAuthToken();
+
+    res.status(201).json({token,captain})
+
+}
+catch(error){
+    console.log(error)
+    return res.status(500).json({
+        message:"error while register captain ",
+        success:false
+    })
+
+}
+
+}
