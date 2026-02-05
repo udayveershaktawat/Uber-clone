1510021c9c7231d838ead615df748ceaaab34ff9
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-05 22:05:31 +0530

    proile route created

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,7 +2,11 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const blackListTokenModel = require("../models/blacklistToken.model");
+const captainModel = require("../models/captain.model")
 
+  
+
+// user Auth
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
@@ -31,3 +35,29 @@ module.exports.authUser = async (req, res, next) => {
     });
   }
 };
+
+
+// captain Auth
+module.exports.authCaptain = async(req,res,next)=>{
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if(!token){
+    return res.status(401).json({message:"unauthorized "})
+  }
+  const isBlacklisted = await blackListTokenModel.findOne({token:token})
+
+  if(isBlacklisted){
+    return res.status(401).json({message:"unauthorized"})
+  }
+
+  try{
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+    const captain = await captainModel.findById(decoded._id);
+    req.captain = captain
+
+  }
+  catch(error){
+    return res.status(401).json({message:"unauthorized"})
+
+  }
+}
