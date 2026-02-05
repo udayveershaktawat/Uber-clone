6a24e025ff91c433854258a9ec9d3988b95a2403
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-05 22:33:35 +0530

    logout controller created

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,3 +1,4 @@
+const blacklistTokenModel = require("../models/blacklistToken.model");
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
 const { validationResult } = require("express-validator");
@@ -85,4 +86,13 @@ module.exports.loginCaptain = async(req,res,next)=>{
 // getProfileCaptain
 module.exports.getCaptainProfile = async(req,res,next)=>{
   res.status(200).json({captain:req.captain})
+}
+
+// logout captain
+module.exports.logoutCaptain = async(req,res,next)=>{
+  const token = req.cookie.token || req.headers.authorization?.split(" ")[1]
+
+  await blacklistTokenModel.create({token})
+  res.clearCookie("token")
+  res.status(200).json({message:"logout successfully"})
 }
