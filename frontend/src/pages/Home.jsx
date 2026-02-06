af0885c7bcd235b71865fb8761b48756707f15fc
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-06 23:58:18 +0530

    u

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
         <div className='bg-white pb-7 py-4 px-4'>
             <h2 className='font-bold text-3xl'>Get started with Uber</h2>
-            <Link className=' flex items-center justify-center w-full bg-black text-white py-3 rounded-md mt-5'>Continue</Link>
+            <Link to='/login' className=' flex items-center justify-center w-full bg-black text-white py-3 rounded-md mt-5'>Continue</Link>
         </div>
 
       </div>
