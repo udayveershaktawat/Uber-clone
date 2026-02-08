f40aa2e596a504b8938b75912f602695f7f278b5
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-09 00:31:37 +0530

    userContext created

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const UserSignup = () => {
-  const [email, setEmail] = uEeState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("")
