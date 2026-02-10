8e9a022d0471ca89daf82b94775e2f63df0c6e41
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-10 23:22:44 +0530

    updated

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap"
+import 'remixicon/fonts/remixicon.css'
 
 const DashBoard = () => {
-    const submitHandler = ()=>{}
+    const [pickup, setPickup] = useState('');
+    const [destination,setDestination]=useState('');
+    const [panelOpen,setPanelOpen]=useState(false);
+    const panelRef = useRef(null)
+
+
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+
+    }
+
+    useGSAP(function(){
+        if(panelOpen){
+
+            gsap.to(panelRef.current,{
+                height:"70%"
+            })
+        }else{
+            gsap.to(panelRef.current,{
+                height:"0%"
+            })
+        }
+    },[panelOpen])
+
+
   return (
     <div className="h-screen relative">
       <img
@@ -9,34 +37,48 @@ const DashBoard = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
         alt=""
       />
-      <div>
+      <div className="h-screen w-screen">
         <img
           className="h-full w-full object-cover"
-          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          src="https://maps.olakrutrim.com/images/features/routing.svg"
           alt=""
         />
       </div>
       <div className=" flex flex-col justify-end  absolute top-0  h-screen w-full">
-        <div className="h-[30%] p-5 relative bg-white">
-          {" "}
+        <div className="h-[30%] p-6 relative bg-white">
+            <h5 onClick={()=>{
+                setPanelOpen(false)
+            }} className="absolute top-6 text-2xl right-6"><i className="ri-arrow-down-wide-line"></i></h5>
+         
           <h4 className="text-2xl font-semibold">Find a trip</h4>
           <form onSubmit={(e)=>{
             submitHandler(e)
           }}>
-            <div className="line absolute h-16 w-8 top-1/2 bg-black"></div>
+             {/* <div className="line absolute h-16 w-1 top-[60%] -translate-y-1/2 left-10  bg-gray-800 rounded-full"></div> */}
             <input
+            onClick={()=>{
+                setPanelOpen(true)
+            }}
+            value={pickup}
+            onChange={(e)=>{
+                setPickup(e.target.value)
+            }}
               className="bg-[#eee] w-full px-12 py-2 rounded-lg mt-5 text-base"
               type="text"
               placeholder="add a pick-up location"
             />
             <input
-              className="bg-[#eee] w-full px-12 py-2 rounded-lg mt-5 text-base"
+            value={destination}
+            onChange={(e)=>{
+                setDestination(e.target.value)
+            }}
+              className="bg-[#eee] w-full px-12 py-2 rounded-lg mt-3 text-base"
               type="text"
               placeholder="Enter yout destination"
             />
           </form>
         </div>
-        <div className=" bg-red-400 h-0"></div>
+        <div ref={panelRef} className=" bg-red-400 h-0"></div>
       </div>
     </div>
   );
