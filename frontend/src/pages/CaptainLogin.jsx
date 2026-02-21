111fad1b26c2a2809e5176fa8a0356cdb115aa07
udayveershaktawat <udayveershaktawat@gmail.com>
2026-02-22 00:30:39 +0530

    u

--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,81 +1,94 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { CaptainDataContext } from "../context/CaptainContext";
 
-
 const CaptainLogin = () => {
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const {captain,setCaptain} = React.useContext(CaptainDataContext)
+  const { captain, setCaptain } = React.useContext(CaptainDataContext);
   // const [captainData , setcaptainData] = useState({})
 
   const navigate = useNavigate();
 
-  const submitHandler=async(e)=>{
+  const submitHandler = async (e) => {
     e.preventDefault();
-    const captain ={
-      email:email,
-      password:password
+    const captain = {
+      email: email,
+      password: password,
+    };
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/captains/login`,
+      captain,
+    );
+
+    if (response.status === 200) {
+      const data = response.data;
+      setCaptain(data.captain);
+      localStorage.setItem("token", data.token);
+      navigate("/captain-home");
     }
-    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/login`,captain)
-
-    if(response.status === 200){
-      const data = response.data
-      setCaptain(data.captain)
-      localStorage.setItem("token",data.token)
-      navigate("/captain-home")
-    }
-    setEmail("")
-    setPassword("")
-
-    
-
-
-
-    
-
-  }
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div className="p-7 h-screen flex flex-col justify-between ">
       <div>
-
-      <img className='w-16 mb-10' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-      <form onSubmit={(e)=>{
-        submitHandler(e)
-      }}>
-        <h3 className="text-xl mb-2 font-semibold">What's your email</h3>
-        <input
-          className="bg-[#eeeeee] mb-7 rounded px-4 border w-full text-lg placeholder:text-base py-2 "
-          required
-          value={email}
-          onChange={(e)=>{
-            setEmail(e.target.value)
-            // console.log(e.target.value)
+        <img
+          className="w-16 mb-10"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt=""
+        />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
           }}
-          type="email"
-          placeholder="email@.gmail.com"
+        >
+          <h3 className="text-xl mb-2 font-semibold">What's your email</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 border w-full text-lg placeholder:text-base py-2 "
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              // console.log(e.target.value)
+            }}
+            type="email"
+            placeholder="email@.gmail.com"
           />
-        <h3 className="text-xl mb-2 font-semibold">Enter Password</h3>
-        <input
-          className="bg-[#eeeeee] mb-7 rounded px-4 border w-full text-lg placeholder:text-base py-2 "
-          required
-          value={password}
-          onChange={(e)=>{
-            setPassword(e.target.value)
-            // console.log(e.target.value)
-          }}
-          type="password"
-          placeholder="password"
+          <h3 className="text-xl mb-2 font-semibold">Enter Password</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 border w-full text-lg placeholder:text-base py-2 "
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              // console.log(e.target.value)
+            }}
+            type="password"
+            placeholder="password"
           />
-        <button className="bg-[#111] text-white mb-3 px-4 rounded-lg w-full text-lg placeholder:text-base py-2 ">Login</button>
-      </form>
-       <p className="text-center"> Join a fleet? <Link to="/captain-signup" className="text-blue-600">Register as a Captain</Link></p>
-          </div>
-          <div>
-            <Link to="/login" className="bg-[#f3c164] flex justify-center items-center text-white mb-5 px-4 rounded-lg w-full text-lg placeholder:text-base py-2 ">Sign in as User</Link>
-          </div>
+          <button className="bg-[#111] text-white mb-3 px-4 rounded-lg w-full text-lg placeholder:text-base py-2 ">
+            Login
+          </button>
+        </form>
+        <p className="text-center">
+          {" "}
+          Join a fleet?{" "}
+          <Link to="/captain-signup" className="text-blue-600">
+            Register as a Captain
+          </Link>
+        </p>
+      </div>
+      <div>
+        <Link
+          to="/login"
+          className="bg-[#f3c164] flex justify-center items-center text-white mb-5 px-4 rounded-lg w-full text-lg placeholder:text-base py-2 "
+        >
+          Sign in as User
+        </Link>
+      </div>
     </div>
   );
 };
