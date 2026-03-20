f5b1be395a8754f502a244c7120020cbf46e38da
udayveershaktawat <udayveershaktawat@gmail.com>
2026-03-20 23:44:27 +0530

    map service created

---
+++ backend/services/maps.service.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+// const captainModel = require('../models/captain.model');
+
+module.exports.getAddressCoordinate = async (address) => {
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
+
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            const location = response.data.results[ 0 ].geometry.location;
+            return {
+                ltd: location.lat,
+                lng: location.lng
+            };
+        } else {
+            throw new Error('Unable to fetch coordinates');
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
