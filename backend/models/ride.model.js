fa8a4c01ac8679543aa5cbf1b12989f1ec482cf0
udayveershaktawat <udayveershaktawat@gmail.com>
2026-03-22 03:13:33 +0530

    otp created

--- backend/models/ride.model.js
+++ backend/models/ride.model.js
@@ -42,7 +42,12 @@ const rideSchema = new mongoose.Schema({
     },
     signature:{
         type:String
-    }
+    },
+      otp: {
+        type: String,
+        select: false,
+        required: true,
+    },
 })
 
 module.exports = mongoose.model("ride",rideSchema)
