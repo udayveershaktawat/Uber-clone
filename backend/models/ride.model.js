5891ea212f4c4dbf13706072b412d52267442d2e
udayveershaktawat <udayveershaktawat@gmail.com>
2026-03-22 02:32:05 +0530

    ride service created

---
+++ backend/models/ride.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const rideSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    captain:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Captain",
+    },
+    pickup:{
+        type:String,
+        required:true
+    },
+    destination:{
+        type:String,
+        required:true
+    },
+    fare:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:['pending','accepted','completed','cancelled'],
+        default:"pending"
+    },
+    duration:{
+        type:Number,
+
+    },
+    distance:{
+        type:Number,
+    },
+    paymentId:{
+        type:String
+    },
+    orderId:{
+        type:String
+    },
+    signature:{
+        type:String
+    }
+})
+
+module.exports = mongoose.model("ride",rideSchema)
