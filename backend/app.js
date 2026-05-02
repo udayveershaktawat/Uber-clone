0bf7eee38fbd911fb81c2dc357e60b810c26fbf0
udayveershaktawat <udayveershaktawat@gmail.com>
2026-05-02 22:56:15 +0530

    ride re uload

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.get("/",(req,res)=>{
 app.use("/users",userRoutes)
 app.use("/captains",captainRoutes)
 app.use('/maps', mapsRoutes);
-app.use('/rides', rideRoutes);
+// app.use('/rides', rideRoutes);
  
 
 
